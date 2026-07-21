f64587a1d7c7f5cceb3ed2b8a7fa6fca2a2c4900
Abhixarvar <abhisarvarshney141204@gmail.com>
2026-07-21 13:56:51 +0530

    Increase express payload size limit to 50mb

--- server/server.js
+++ server/server.js
@@ -17,7 +17,8 @@ const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGODB_URI)
